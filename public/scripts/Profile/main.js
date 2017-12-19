1ea80ffad2bac0505c35b483798dd3753e25d66c
Aaron Goshine <greenlig@gmail.com>
2017-12-19 13:15:14 +0000

    fix(Eslint): fixed Eslint errors and improved code quality

--- public/scripts/Profile/main.js
+++ public/scripts/Profile/main.js
@@ -1,20 +1,20 @@
-console.log('this is a profile page');
+/* global $ */
 
 $.get('/profile-info/01', (data) => {
-    $('#username').text(data.username);
+  $('#username').text(data.username);
 });
 
 $.get('/watched-list/01', (data) => {
-    const watched = data.watched;
-    for(i = 0; i < watched.length; i++) {
-        $('#watched-list').append($('<li>' + watched[i].title + '</li>'));
-    };
+  const watched = data.watched;
+  for (var i = 0; i < watched.length; i++) {
+    $('#watched-list').append($('<li>' + watched[i].title + '</li>'));
+  };
 });
 
 $.get('/towatch-list/01', (data) => {
-    const toWatch = data.toWatch;
-    for(i = 0; i < toWatch.length; i++) {
-        $('#to-watch-list').append($('<li>' + toWatch[i].title + '</li>'));
-    };
+  const toWatch = data.toWatch;
+  for (var i = 0; i < toWatch.length; i++) {
+    $('#to-watch-list').append($('<li>' + toWatch[i].title + '</li>'));
+  };
 });
 
